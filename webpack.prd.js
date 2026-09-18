6fd07b8443fb0ac2418965fb464178f11593a5b3
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-09-18 18:31:02 +0200

    chore: bump node to >=22 and fix webpack-merge v6 usage

--- webpack.prd.js
+++ webpack.prd.js
@@ -1,9 +1,9 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
-module.exports = merge.smart({
+module.exports = merge({
     mode: 'production',
     devtool: 'source-map',
     module: {
